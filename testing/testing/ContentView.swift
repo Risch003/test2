8fb5e1c4a1838ab53da10e5f8b2fb4efde562235
Cameron Risch <risch.cameron@gmail.com>
2020-02-21 00:05:54 -0600

    2nd commit

--- testing/testing/ContentView.swift
+++ testing/testing/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
